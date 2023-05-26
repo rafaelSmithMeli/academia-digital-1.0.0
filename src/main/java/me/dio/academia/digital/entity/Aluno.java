8e281225c917bc180b8b649d5ffcdcedd21bce0d
Rafael Smith <rafael.smith@mercadolivre.com>
2023-05-26 10:02:17 -0300

    create methods createAvaliacaoFisica and createAluno

--- src/main/java/me/dio/academia/digital/entity/Aluno.java
+++ src/main/java/me/dio/academia/digital/entity/Aluno.java
@@ -10,26 +10,27 @@ import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "tb_alunos")
-@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Aluno {
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  private String nome;
-@Column(unique = true)
-  private String cpf;
+    private String nome;
+    @Column(unique = true)
+    private String cpf;
 
-  private String bairro;
+    private String bairro;
 
-  private LocalDate dataDeNascimento;
-@OneToMany(mappedBy ="aluno",fetch = FetchType.LAZY)
-@JsonIgnore
-  private List<AvaliacaoFisica> avaliacoes = new ArrayList<>();
+    private LocalDate dataDeNascimento;
+    @OneToMany(mappedBy = "aluno", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<AvaliacaoFisica> avaliacoes = new ArrayList<>();
 
 }
--- src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
@@ -1,7 +1,14 @@
 package me.dio.academia.digital.entity.form;
 
-import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NegativeOrZero;
+import java.time.LocalDate;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AlunoForm {
 
   private String nome;
